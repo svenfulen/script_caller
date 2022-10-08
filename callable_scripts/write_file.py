ad740dfe358fd30375bc096ee4efd955d5046029
Sven Fulenchek <73049985+svenfulen@users.noreply.github.com>
2022-10-08 13:38:15 -0500

    added comments

--- callable_scripts/write_file.py
+++ callable_scripts/write_file.py
@@ -1,23 +1,30 @@
+# Example script that writes to a file given arguments
+
 import sys
 
+# filename is the file to write to
+# text is the text to write to the file
 filename = None
 text = None
 
+# Check that the right number of arguments were given
 if len(sys.argv) < 4:
     print("Usage: write_file.py --filename [filename] --text [text]")
     sys.exit(1)
 
-filename = sys.argv[1]
-
+# Get the arguments from the command line
 for idx, arg in enumerate(sys.argv):
     if arg == "--text":
         text = sys.argv[idx + 1]
     elif arg == "--filename":
         filename = sys.argv[idx + 1]
 
+# Provide output to the console
+print("Writing file...")
 print("filename: " + filename)
 print("text: " + str(text))
 
+# Write the file
 f = open(("./callable_scripts/" + filename), "a")
 f.write(text)
 f.close()
--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from flask_restful import Api, Resource
 
 from subprocess import Popen
 
-
+# Create the Flask app
 app = Flask(__name__)
 api = Api(app)
 
+
+# API Endpoint to run a script
 class RunScript(Resource):
     def post(self, script_name):
+        
+        # Create the arguments for the script in both array and string format
         arguments_json = request.get_json()
         arguments_array = []
         arguments_string = ""
@@ -18,10 +22,14 @@ class RunScript(Resource):
             arguments_array.append(value)
             arguments_string += (arg + " " + value + " ")
         
-        Popen(("python " + "callable_scripts/" + script_name + " " + arguments_string), shell=True)
-
-        return Response("OK", status=200)
-
+        
+        # Try to run the script in a new process with the given arguments
+        try:
+            Popen(("python " + "callable_scripts/" + script_name + " " + arguments_string), shell=True)
+            return Response("OK", status=200)
+        except:
+            return Response("Script failed to run", status=500)
+    
 
 api.add_resource(RunScript, "/runscript/<string:script_name>")
 
